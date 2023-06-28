63b45550348094416c0bb566788dec7620d5d797
dujing <dujing@xmov.ai>
2023-06-28 14:08:49 +0800

    add xiaowen model and examples.

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ import gradio as gr
 import wave
 from stream_kws_ctc import KeyWordSpotter
 
-kws = KeyWordSpotter(ckpt_path='model/nihaoxiaojing/avg_30.pt',
+kws_xiaojing = KeyWordSpotter(ckpt_path='model/nihaoxiaojing/avg_30.pt',
                      config_path='model/nihaoxiaojing/config.yaml',
-                     token_path='model/nihaoxiaojing/tokens.txt',
-                     lexicon_path='model/nihaoxiaojing/lexicon.txt',
+                     token_path='model/tokens.txt',
+                     lexicon_path='model/lexicon.txt',
                      threshold=0.02,
                      min_frames=5,
                      max_frames=150,
@@ -29,11 +29,32 @@ kws = KeyWordSpotter(ckpt_path='model/nihaoxiaojing/avg_30.pt',
                      gpu=-1,
                      is_jit_model=False,)
 
-# actually this could be done in __init__ method, we pull it outside for changing keywords more freely.
-kws.set_keywords("你好小镜")
+kws_xiaojing.set_keywords("你好小镜")
 
+kws_xiaowen = KeyWordSpotter(ckpt_path='model/hixiaowen/avg_30.pt',
+                     config_path='model/haixioawen/config.yaml',
+                     token_path='model/tokens.txt',
+                     lexicon_path='model/lexicon.txt',
+                     threshold=0.02,
+                     min_frames=5,
+                     max_frames=150,
+                     interval_frames=50,
+                     score_beam=3,
+                     path_beam=20,
+                     gpu=-1,
+                     is_jit_model=False,)
 
-def detection(audio):
+kws_xiaowen.set_keywords("嗨小问,你好问问")
+
+def detection(audio, kw):
+    if kw=='nihaoxiaojing':
+        kws=kws_xiaojing
+    elif kw=='hixiaowen' or kw=='nihaowenwen':
+        kws=kws_xiaowen
+
+    else:  # for other input data, we recommend xiaowen model
+       kws=kws_xiaowen
+
     kws.reset_all()
     if audio is None:
         return "Input Error! Please enter one audio!"
@@ -60,26 +81,31 @@ def detection(audio):
 # input
 inputs = [
     gr.inputs.Audio(source="microphone", type="filepath", label='Input audio'),
+    gr.Radio(['nihaoxiaojing', 'hixiaowen', 'nihaowenwen', 'none'], label='kw')
 ]
 
 output = gr.outputs.Textbox(label="Output Result")
 
 examples = [
-    ['examples/chentianbo_ceqianfang_anjing_0001.wav'],
-    ['examples/chentianbo_ceqianfang_neiwaizao_0001.wav'],
-    ['examples/chentianbo_ceqianfang_neizao_0001.wav'],
-    ['examples/chentianbo_ceqianfang_waizao_0001.wav'],
-    ['examples/gongqu-4.5_0000.wav'],
-    ['examples/neiwaizao-35.5h_0000.wav'],
-    ['examples/neizao-4.5h_0000.wav'],
-    ['examples/waizao-5.5h_0000.wav'],
+    ['examples/chentianbo_ceqianfang_anjing_0001.wav', 'nihaoxiaojing'],
+    ['examples/chentianbo_ceqianfang_neiwaizao_0001.wav', 'nihaoxiaojing'],
+    ['examples/chentianbo_ceqianfang_neizao_0001.wav', 'nihaoxiaojing'],
+    ['examples/chentianbo_ceqianfang_waizao_0001.wav', 'nihaoxiaojing'],
+    ['examples/gongqu-4.5_0000.wav', 'none'],
+    ['examples/neiwaizao-35.5h_0000.wav', 'none'],
+    ['examples/neizao-4.5h_0000.wav', 'none'],
+    ['examples/waizao-5.5h_0000.wav', 'none'],
+    ['examples/0000c7286ebc7edef1c505b78d5ed1a3.wav', 'nihaowenwen'],
+    ['examples/0000e12e2402775c2d506d77b6dbb411.wav', 'nihaowenwen'],
+    ['examples/000af5671fdbaa3e55c5e2bd0bdf8cdd.wav', 'hixiaowen'],
+    ['examples/000eae543947c70feb9401f82da03dcf.wav', 'hixiaowen'],
 ]
 
 text = "Key Word Spotting | 关键词检测"
 
 # description
 description = (
-    "KWS Demo of Nihao_Xiaojing! This is a key word spotting demo supports \'nihaoxiaojing\' detection !"
+    "KWS Demo! 'nihaoxiaojing' model is for farfield, 'hixiaowen' and 'nihaowenwen' is for nearfield."
 )
 
 interface = gr.Interface(
